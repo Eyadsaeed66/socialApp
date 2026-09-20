ca053c4f516be29075698288a836e8ae6b57054b
Eyadsaeed66 <saeedeyad713@gmail.com>
2026-09-20 19:50:46 +0300

    profie page

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css',
+  styleUrl:'./profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  currentUser: any = null;
+
+  defaultAvatar = 'assets/images/default-avatar.png';
+
+  posts = signal<any[]>([]);
+
+  loading = signal<boolean>(false);
+
+  errorMessage = signal<string>('');
+
+  ngOnInit(): void {
+    // هنضيف جلب الـ user والـ posts هنا بعدين
+  }
+
+  editPost(post: any): void {
+    console.log('Edit post:', post);
+  }
+
+  deletePost(postId: string): void {
+    console.log('Delete post:', postId);
+  }
 }
